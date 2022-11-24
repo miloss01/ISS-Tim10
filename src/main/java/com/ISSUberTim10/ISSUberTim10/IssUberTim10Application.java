a01e049a21fae33bb1d7346a3d6725814cd2e5a0
unknown <spremic.katarina@gmail.com>
2022-11-24 19:13:30 +0100

    Initialize project structure

---
+++ src/main/java/com/ISSUberTim10/ISSUberTim10/IssUberTim10Application.java
@@ -0,0 +1,13 @@
+package com.ISSUberTim10.ISSUberTim10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssUberTim10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssUberTim10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ISSUberTim10/ISSUberTim10/IssUberTim10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ISSUberTim10.ISSUberTim10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssUberTim10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
